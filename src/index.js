585e84510fea30163cb894e72a1d509e23666204
Lucas <lucas>
2022-07-13 17:58:20 -0300

    Commit

--- src/index.js
+++ src/index.js
@@ -61,7 +61,6 @@ function findUserById(request, response, next) {
   const { id } = request.params
 
   const user = users.find((user) => user.id === id);
-
   if (!user) {
     return response.status(404).json({ error: 'User ID not found!' });
   }
